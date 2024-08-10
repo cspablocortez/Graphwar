3e8084a6f2ca7671d687d8ec2a94cf4209a6f121
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-08-10 17:32:31 -0400

    updated file

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
+let func = ''; 
+let input; 
+let button; 
+
+
 function setup() {
-    createCanvas(600, 400);
-    background("#264cbf");
-  }
+    createCanvas(600, 500);
+
+    input = select('#functionInput');
+    button = select('#plotButton');
+    button.mousePressed(sanitizeInput); 
+    
+}
   
   function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
+
+    stroke(255);
+    line(0, height / 2, width, height / 2);                   // X-axis
+    line(width / 2, 0, width / 2, height);                    // Y-axis
+
+    if (func) {
+      plot();
     }
-    ellipse(mouseX, mouseY, 10, 10);
   }
+
+function sanitizeInput() {
+  func = input.value().replace('^', '**').replace(/ln/g, 'Math.log'); 
+}
+
+function plot() {
+  noFill();
+  stroke(0, 151, 101);
+
+  beginShape();
+  for (let x = -width / 2; x < width / 2; x++) {
+      let xCoord = x + width / 2;
+      let y;
+      try {
+          y = eval(func.replace(/x/g, `(${x})`));
+          vertex(xCoord, height / 2 - y);
+      } catch (e) {
+          console.error("Invalid function input", e);
+          break;
+      }
+  }
+  endShape();
+  
+}
